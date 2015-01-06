f1a15e597b0fcf1a51aa666d610f2e3ae1332b77
Mark Fickett <mark.fickett@gmail.com>
2015-01-05 23:47:31 -0500

    Switch to custom event loop and included nameserver, following Pyro4 examples/eventloop/server.py.

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-# http://pythonhosted.org/Pyro4/intro.html#simple-example
-# Start name server with: python -m Pyro4.naming
-
 import random
+import select
+import socket
 
 import Pyro4
 
 import common
 import messages_pb2  # generate with: protoc --python_out=. messages.proto
+
+
+_SOCKET_READ_TIMEOUT = 3.0  # 0 for non-blocking
 
 
 class Server(object):
@@ -52,6 +54,32 @@ class Server(object):
 
 if __name__ == '__main__':
   common.RegisterProtoSerialization()
-  Pyro4.core.Daemon.serveSimple(
-      {Server(): common.SERVER_URI_NAME},
-      ns=True)
+
+  hostname = socket.gethostname()
+  ip_addr = Pyro4.socketutil.getIpAddress(None, workaround127=True)
+  ns_uri, ns_daemon, broadcast_server = Pyro4.naming.startNS(host=ip_addr)
+  assert broadcast_server
+  pyro_daemon = Pyro4.core.Daemon(host=hostname)
+  server_uri = pyro_daemon.register(Server())
+  ns_daemon.nameserver.register(common.SERVER_URI_NAME, server_uri)
+  print 'registered', common.SERVER_URI_NAME, server_uri
+
+  try:
+    ns_sockets = set(ns_daemon.sockets)
+    pyro_sockets = set(pyro_daemon.sockets)
+    sockets_to_read = (
+        [broadcast_server] + ns_daemon.sockets + pyro_daemon.sockets)
+    while True:
+      ready_read_sockets, _, _ = select.select(
+          sockets_to_read, (), (), _SOCKET_READ_TIMEOUT)
+      for s in ready_read_sockets:
+        if s in pyro_sockets:
+          pyro_daemon.events((s,))
+        elif s in ns_sockets:
+          ns_daemon.events((s,))
+        elif s is broadcast_server:
+          broadcast_server.processRequest()
+  finally:
+    ns_daemon.close()
+    broadcast_server.close()
+    pyro_daemon.close()
