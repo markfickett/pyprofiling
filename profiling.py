6b73a941f8152bb090b0af46e98fe7430071af4b
Mark Fickett <mark.fickett@gmail.com>
2015-01-24 22:32:15 -0500

    Add context guard for profiling.

---
+++ profiling.py
@@ -0,0 +1,57 @@
+import collections
+import time
+
+
+_REPORT_INTERVAL_S = 10.0
+_INDENT = '  '
+_Report = collections.namedtuple(
+    'Report', ('is_root', 'name', 'children', 'durations'))
+
+
+class Profiled(object):
+  _stack = []
+  _last_report_time = time.time()
+  _reports = {}
+
+  def __init__(self, name):
+    self._report = self._reports.get(name)
+    if not self._report:
+      self._report = _Report(
+          is_root=not self._stack,
+          name=name,
+          children=[],
+          durations=[])
+      if self._stack:
+        self._stack[-1].children.append(self._report)
+      self._reports[name] = self._report
+
+  def __enter__(self):
+    self._start = time.time()
+    self._stack.append(self._report)
+
+  def __exit__(self, excClass, excObj, tb):
+    self._report.durations.append(time.time() - self._start)
+    self._stack.pop()
+    self._MaybePrintReport()
+
+  @classmethod
+  def _MaybePrintReport(cls):
+    if (cls._stack or
+        time.time() - Profiled._last_report_time < _REPORT_INTERVAL_S):
+      return
+    cls._last_report_time = time.time()
+    for report in cls._reports.values():
+      if report.is_root:
+        cls._PrintReport(report, 0)
+    cls._reports = {}
+
+  @classmethod
+  def _PrintReport(cls, report, level):
+    print (
+        '%s%s %d @ %.2fs' %
+        (level * _INDENT,
+         report.name,
+         len(report.durations),
+         sum(report.durations) / len(report.durations)))
+    for child in report.children:
+      cls._PrintReport(child, level + 1)
